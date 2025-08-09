e325d49b886865edbf706cf5b2d09590500c71ec
Charly Kouakou <85646893+Charlythegreat@users.noreply.github.com>
2025-08-09 03:39:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   // Ensure assets are served under the GitHub Pages project path
   base: '/kouakou-portfolio/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
